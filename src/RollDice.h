e9c175e00f1ecbde17cbbb53e8a64a52b1a49033
w4123 <34530484+w4123@users.noreply.github.com>
2018-01-29 22:34:08 +0800

    Update RollDice.h

--- src/RollDice.h
+++ src/RollDice.h
@@ -4,9 +4,12 @@
 extern inline int MainRoll(int&, std::string, std::string&, std::string&);
 extern inline int MainRoll(int&, std::string);
 extern inline void COC6(std::string&);
+extern inline void COC6S(std::string&, int);
 extern inline void COC7(std::string&);
-extern inline void DND(std::string&); 
+extern inline void COC7S(std::string&,int);
+extern inline void DND(std::string&,int); 
 extern inline void LongInsane(std::string&);
 extern inline void TempInsane(std::string&);
 extern std::string strRules;
+extern inline std::string ImagePack();
 #endif
